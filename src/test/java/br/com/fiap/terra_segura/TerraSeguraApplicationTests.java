b0289f854356f26552f8d06f2da901cefe0c465d
Caepena <caepenafiel@gmail.com>
2025-06-05 12:45:37 -0300

    Init

---
+++ src/test/java/br/com/fiap/terra_segura/TerraSeguraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.terra_segura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraSeguraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
